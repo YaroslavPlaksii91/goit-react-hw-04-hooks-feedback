f46778ed7cb3978ffb0f6177568b7482723f6b51
YaroslavPlaksii91 <yarosyk@ukr.net>
2022-05-21 10:57:09 +0300

    delete class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,54 +4,6 @@ import Statistics from './Statistics';
 import FeedbackOptions from './FeedbackOptions';
 import Notification from './Notification';
 
-// class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   countTotalFeedback = () =>
-//     Object.values(this.state).reduce((total, value) => total + value);
-
-//   countPositiveFeedbackPercentage = () =>
-//     Math.round((this.state.good / this.countTotalFeedback()) * 100);
-
-//   changeStateOnFeedback = option => {
-//     this.setState(prevState => {
-//       return { [option]: prevState[option] + 1 };
-//     });
-//   };
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-
-//     return (
-//       <div>
-//         <Section title="Please leave feedback">
-//           <FeedbackOptions
-//             options={Object.keys(this.state)}
-//             onLeaveFeedback={this.changeStateOnFeedback}
-//           />
-//         </Section>
-//         <Section title="Statistics">
-//           {this.countTotalFeedback() ? (
-//             <Statistics
-//               good={good}
-//               neutral={neutral}
-//               bad={bad}
-//               total={this.countTotalFeedback()}
-//               positivePercentage={this.countPositiveFeedbackPercentage()}
-//             />
-//           ) : (
-//             <Notification message="There is no feedback" />
-//           )}
-//         </Section>
-//       </div>
-//     );
-//   }
-// }
-
 export const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -74,20 +26,8 @@ export const App = () => {
         setBad(prev => prev + 1);
         break;
       default:
-        console.log('No such option');
+        return;
     }
-
-    // if (option === 'good') {
-    //   setGood(prev => prev + 1);
-    // }
-
-    // if (option === 'neutral') {
-    //   setNeutral(prev => prev + 1);
-    // }
-
-    // if (option === 'bad') {
-    //   setBad(prev => prev + 1);
-    // }
   };
 
   return (
